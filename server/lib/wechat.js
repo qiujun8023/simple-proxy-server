cf8ba6c95c26d5e7563a5e36d069ca0f459e959b
邱俊 <i@qiujun.me>
2017-04-06 09:45:05 +0800

    fix: 修复微信登录失败的 Bug

--- server/lib/wechat.js
+++ server/lib/wechat.js
@@ -7,20 +7,21 @@ const redis = require('../lib/redis');
 
 const TOKEN_CACHE_KEY = 'wechat:access_token';
 
-let wechat = module.exports = {};
-
-wechat._getToken = function (callback) {
+let getToken = function (callback) {
   redis.get(TOKEN_CACHE_KEY, function (err, result) {
     callback(err, JSON.parse(result));
   });
 };
 
-wechat._setToken = function (token, callback) {
+let setToken = function (token, callback) {
   let time = token.expiresIn - 600;
   let data = JSON.stringify(token);
   redis.setex(TOKEN_CACHE_KEY, time, data, callback);
 };
 
 let {corp_id, secret} = config.wechat;
-let api = new API(corp_id, secret, 0, wechat._getToken, wechat._setToken);
-wechat = Promise.promisifyAll(api);
+let api = new API(corp_id, secret, 0, getToken, setToken);
+let wechat = module.exports = Promise.promisifyAll(api);
+
+wechat._getToken = getToken;
+wechat._setToken = setToken;

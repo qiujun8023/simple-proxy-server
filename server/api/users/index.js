ea3c0e2ee257ec9014ef45979082969dce35ed95
邱俊 <i@qiujun.me>
2017-04-07 15:40:29 +0800

    feat: 增加用户相关接口

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -6,12 +6,27 @@ const {User} = require('../../service');
 
 let router = module.exports = express.Router();
 
-router.get('/', function* (req, res) {
+router.use(function (req, res, next) {
   if (!req.session.user.is_admin) {
     throw new HttpError(HttpError.FORBIDDEN, '无权访问');
   }
+  next();
+});
 
+router.get('/', function* (req, res) {
   let {key} = req.query;
   let users = yield User.searchAsync(key);
   res.send(users);
+});
+
+router.get('/:user_id', function* (req, res) {
+  let {user_id} = req.params;
+  let user = yield User.getAsync(user_id);
+  res.send(user);
+});
+
+router.put('/:user_id', function* (req, res) {
+  let {user_id} = req.params;
+  let user = yield User.updateAsync(user_id, req.body);
+  res.send(user);
 });
